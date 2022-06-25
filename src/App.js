5a23b7965194fe567bf57e8e569cf349ca1e2445
Shea <ss1316@duke.edu>
2022-06-25 19:42:24 -0400

    init

--- src/App.js
+++ src/App.js
@@ -1,11 +1,57 @@
 // import logo from './logo.svg';
 import './App.css';
-import Header from "./list";
+import React from 'react';
+import Select from 'react-select';
 
-function App() {
+const semesters = [
+  { value: 'Spring 2023', label: 'Spring 2023' },
+  { value: 'Fall 2022', label: 'Fall 2022' },
+  { value: 'Summer 2022', label: 'Summer 2022' },
+];
+const graduates = [
+  { value: 'Graduate', label: 'Graduate' },
+  { value: 'Undergraduate', label: 'Undergraduate' },
+];
 
-  return (
-    <Header title = 'Home'/>
+class App extends React.Component {
+  state = {
+    selectedOption: null,
+  };
+  handleChange = (selectedOption) => {
+    this.setState({ selectedOption }, () =>
+      console.log(`Option selected:`, this.state.selectedOption)
+    );
+  };
+  render() {
+    const { selectedOption } = this.state;
+
+    return (
+      <>
+      <span>Semester</span>
+      <Select defaultValue={"Semester"} size="small"
+        value={selectedOption}
+        onChange={this.handleChange}
+        options={semesters}
+      />
+      <span >Graduate/Undergraduate</span>
+      <Select defaultValue={"Graduate"} size="small"
+        value={selectedOption}
+        onChange={this.handleChange}
+        options={graduates}
+      />
+      <span>Table Name</span>
+      <br>
+      </br>
+      <input name='table name' type='text' required></input>
+      <div>
+  <button>Start</button>
+  </div>
+      </>
+    );
+  }
+}
+
+
     // <div className="App">
     //   <header className="App-header">
     //     <img src={logo} className="App-logo" alt="logo" />
@@ -22,7 +68,7 @@ function App() {
     //     </a>
     //   </header>
     // </div>
-  );
-}
+//   );
+// }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  // 去掉严格模式节点
+  
   // <React.StrictMode>
     <App />
   // </React.StrictMode>
