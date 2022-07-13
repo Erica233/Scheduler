3edade80ab7b4fc7be2c664c161c9abd83bf76ad
Shea <ss1316@duke.edu>
2022-07-13 16:46:14 -0400

    add router

---
+++ src/pages/EditedTable.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const EditedTable = () => {
+ return(
+    <div>
+        <p>
+            edited pages
+        </p>
+    </div>
+ );
+}
+
+export default EditedTable;
