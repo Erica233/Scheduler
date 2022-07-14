39e6db578d04f76b4953caf272965d66dbb297a8
Shea <ss1316@duke.edu>
2022-07-13 23:20:46 -0400

    install table

--- src/pages/EditedTable.jsx
+++ src/pages/EditedTable.jsx
@@ -1,12 +1,23 @@
 import React from "react";
+import { Table, Column, HeaderCell, Cell } from 'rsuite-table';
+
 
 const EditedTable = () => {
  return(
-    <div>
-        <p>
-            edited pages
-        </p>
-    </div>
+<Table>
+    <Column  width={100} sort fixed resizable>
+        <HeaderCell>Week</HeaderCell>
+        <Cell dataKey="week" />
+    </Column>
+    <Column  width={100} sort resizable>
+        <HeaderCell>Topic</HeaderCell>
+        <Cell dataKey="topic" />
+    </Column>
+    <Column  width={100} sort resizable>
+        <HeaderCell>Description</HeaderCell>
+        <Cell dataKey="description" />
+    </Column>
+</Table>
  );
 }
 
