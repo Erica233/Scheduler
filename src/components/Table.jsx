9fefbb0e89e62d1818166880ff4a9a2576a2e0d7
Shea <ss1316@duke.edu>
2022-07-19 14:53:27 -0400

    edited table

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,54 +1,194 @@
-import * as React from 'react';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
+import React, { useState } from 'react';
+import 'antd/dist/antd.css';
+// import './index.css';
+import { Form, Input, InputNumber, Popconfirm, Table, Typography } from 'antd';
 
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
+const originData = [];
+
+for (let i = 1; i < 10; i++) {
+  originData.push({
+    key: i.toString(),
+    week: `${i}`,
+    date: `${i}`,
+    topic: ``,
+    description: ``,
+  });
 }
 
-const rows = [
-  createData(1, '01/01 MON', 'N/A', 'N/A', 'N/A'),
-  createData(2, '01/08 MON', 'N/A', 'N/A', 'N/A'),
-  createData(3, '01/15 MON', 'N/A', 'N/A', 'N/A'),
-  createData(4, '01/22 MON', 'N/A', 'N/A', 'N/A'),
-  createData(5, '01/29 MON', 'N/A', 'N/A', 'N/A'),
-];
-
-export default function BasicTable() {
+const EditableCell = ({
+  editing,
+  dataIndex,
+  title,
+  inputType,
+  record,
+  index,
+  children,
+  ...restProps
+}) => {
+  const inputNode = inputType === 'number' ? <InputNumber /> : <Input />;
   return (
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>Week</TableCell>
-            <TableCell align="right">Date</TableCell>
-            <TableCell align="right">Topic</TableCell>
-            <TableCell align="right">Description</TableCell>
-            <TableCell align="right">Other</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map((row) => (
-            <TableRow
-              key={row.name}
-              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-            >
-              <TableCell component="th" scope="row">
-                {row.name}
-              </TableCell>
-              <TableCell align="right">{row.calories}</TableCell>
-              <TableCell align="right">{row.fat}</TableCell>
-              <TableCell align="right">{row.carbs}</TableCell>
-              <TableCell align="right">{row.protein}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+    <td {...restProps}>
+      {editing ? (
+        <Form.Item
+          name={dataIndex}
+          style={{
+            margin: 0,
+          }}
+          rules={[
+            {
+              required: true,
+              message: `Please Input ${title}!`,
+            },
+          ]}
+        >
+          {inputNode}
+        </Form.Item>
+      ) : (
+        children
+      )}
+    </td>
   );
-}
+};
+
+const BasicTable = () => {
+  const [form] = Form.useForm();
+  const [data, setData] = useState(originData);
+  const [editingKey, setEditingKey] = useState('');
+
+  const isEditing = (record) => record.key === editingKey;
+
+  const edit = (record) => {
+    form.setFieldsValue({
+      week: '',
+      date: '',
+      topic: '',
+      description: '',
+      ...record,
+    });
+    setEditingKey(record.key);
+  };
+
+  const cancel = () => {
+    setEditingKey('');
+  };
+
+  const save = async (key) => {
+    try {
+      const row = await form.validateFields();
+      const newData = [...data];
+      const index = newData.findIndex((item) => key === item.key);
+
+      if (index > -1) {
+        const item = newData[index];
+        newData.splice(index, 1, { ...item, ...row });
+        setData(newData);
+        setEditingKey('');
+      } else {
+        newData.push(row);
+        setData(newData);
+        setEditingKey('');
+      }
+    } catch (errInfo) {
+      console.log('Validate Failed:', errInfo);
+    }
+  };
+
+  const columns = [
+    {
+      title: 'week',
+      dataIndex: 'week',
+      width: '15%',
+      editable: true,
+    },
+    {
+      title: 'date',
+      dataIndex: 'date',
+      width: '25%',
+      editable: true,
+      // sorter: (a, b) => {
+      //   //将日期转成毫秒数，有利于计算大小
+      //     let atime=new Date(a.Date.replace(/-/g,'/')).getTime();
+      //     let btime=new Date(b.Date.replace(/-/g,'/')).getTime();
+      //     return atime - btime
+      //   },
+      //   //两个排序方向
+      //   sortDirections: ['descend', 'ascend'],
+      //   //默认排序
+      //   defaultSortOrder: 'descend',
+    },
+    {
+      title: 'topic',
+      dataIndex: 'topic',
+      width: '20%',
+      editable: true,
+    },
+    {
+      title: 'description',
+      dataIndex: 'description',
+      width: '20%',
+      editable: true,
+    },
+    {
+      title: 'operation',
+      dataIndex: 'operation',
+      render: (_, record) => {
+        const editable = isEditing(record);
+        return editable ? (
+          <span>
+            <Typography.Link
+              onClick={() => save(record.key)}
+              style={{
+                marginRight: 8,
+              }}
+            >
+              Save
+            </Typography.Link>
+            <Popconfirm title="Sure to cancel?" onConfirm={cancel}>
+              <a>Cancel</a>
+            </Popconfirm>
+          </span>
+        ) : (
+          <Typography.Link disabled={editingKey !== ''} onClick={() => edit(record)}>
+            Edit
+          </Typography.Link>
+        );
+      },
+    },
+  ];
+  const mergedColumns = columns.map((col) => {
+    if (!col.editable) {
+      return col;
+    }
+
+    return {
+      ...col,
+      onCell: (record) => ({
+        record,
+        inputType: col.dataIndex === 'week' ? 'number' : 'text',
+        dataIndex: col.dataIndex,
+        title: col.title,
+        editing: isEditing(record),
+      }),
+    };
+  });
+  return (
+    <Form form={form} component={false}>
+      <Table
+        components={{
+          body: {
+            cell: EditableCell,
+          },
+        }}
+        bordered
+        dataSource={data}
+        columns={mergedColumns}
+        rowClassName="editable-row"
+        pagination={{
+          onChange: cancel,
+        }}
+      />
+    </Form>
+  );
+};
+
+export default BasicTable;
