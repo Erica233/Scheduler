362331ef29a7b20034f0bb527ab50be26d25487f
Shea <ss1316@duke.edu>
2022-07-25 17:04:16 -0400

    edit calculating week num module

--- src/components/RowForm.jsx
+++ src/components/RowForm.jsx
@@ -20,16 +20,24 @@ function RowForm() {
         console.log(event);
     }
 
+
+    var inputDate = new Date(inputField.date);
+    var oneJan = new Date(inputDate.getFullYear(), 0, 1);
+    var numberOfDays = Math.floor((inputDate - oneJan) / (24 * 60 * 60 * 1000));
+    var result = Math.ceil(( inputDate.getDay() + 1 + numberOfDays) / 7);
+    inputField.week = result;
+    console.log(`Week Number (${inputDate}) is ${inputField.week}.`);
+
     return (
         <div>
-            <form>
-                <input
+            <form name="addRow">
+                {/* <input
                   type="number" required
+                  min="1" max="99"
                   onChange={weekHandler}
                   value={inputField.week}
                   placeholder="Week"
-                  name="week" />
-                <br/>
+                  name="week" /> */}
                 <input
                   type="date" required
                   onChange={dateHandler}
