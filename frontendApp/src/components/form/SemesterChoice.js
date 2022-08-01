33f19cc5b743c669a6200b4d04d129ec11e232ca
ay135 <ay135@duke.edu>
2022-08-01 19:21:15 -0400

    Upload New File

---
+++ frontendApp/src/components/form/SemesterChoice.js
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import Select from "react-select";
+import "../../App.css";
+import styled from "styled-components";
+
+const semester_options = [
+  { value: "spring", label: "Spring" },
+  { value: "summer1", label: "Summer Term 1" },
+  { value: "summer2", label: "Summer Term 2" },
+  { value: "fall", label: "Fall" },
+];
+
+const SemesterChoice = (props) => {
+  const [semesterOption, setSemesterOption] = React.useState(null);
+
+  // get selected value
+  const dropdownChangeHandler = (option) => {
+    setSemesterOption(option);
+    props.onChangeFilter(option.value);
+  };
+
+  return (
+    <div className="row">
+      <label>{props.description}</label>
+      <SelectsContainer>
+        <Select
+          className="select"
+          size="small"
+          placeholder="Please Select"
+          options={semester_options}
+          onChange={dropdownChangeHandler}
+        />
+      </SelectsContainer>
+    </div>
+  );
+};
+
+const SelectsContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  height: 20%;
+  width: 100%;
+`;
+
+export default SemesterChoice;
