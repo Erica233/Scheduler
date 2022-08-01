19c60991324dd3d6a2ac3b9bc480da38aed49228
ay135 <ay135@duke.edu>
2022-08-01 19:20:58 -0400

    Upload New File

---
+++ frontendApp/src/components/form/Identity.js
@@ -0,0 +1,45 @@
+import React, {useState} from "react";
+import Select from "react-select";
+import styled from "styled-components";
+import "../../App.css";
+
+const grade_options = [
+  { value: "graduate", label: "Graduate" },
+  { value: "undergraduate", label: "Undergraduate" },
+];
+
+const Identity = (props) => {
+  const [gradeOption, setGradeOption] = useState(null);
+
+  const dropdownChangeHandler = (option) => {
+    setGradeOption(option);
+    props.onChangeFilter(option.value);
+  }
+
+  return (
+    <div className="row">
+      <label>{props.description}</label>
+      <SelectsContainer>
+<Select
+        className="select"
+        size="small"
+        placeholder="Please Select"
+        options={grade_options}
+        onChange={dropdownChangeHandler}
+      />
+      </SelectsContainer>
+         
+     
+    </div>
+  );
+};
+
+const SelectsContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  height: 20%;
+  width: 100%;
+`;
+
+export default Identity;
