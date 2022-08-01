b2a784daa0857b3cf5eb0c653e184df11fae2848
ay135 <ay135@duke.edu>
2022-08-01 19:20:47 -0400

    Upload New File

---
+++ frontendApp/src/components/form/Day.js
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+import Select from "react-select";
+import makeAnimated from "react-select/animated";
+import styled from "styled-components";
+import "../../App.css";
+
+const day_options = [
+  { value: 0, label: "Sunday" },
+  { value: 1, label: "Monday" },
+  { value: 2, label: "Tuesday" },
+  { value: 3, label: "Wednesday" },
+  { value: 4, label: "Thursday" },
+  { value: 5, label: "Friday" },
+  { value: 6, label: "Saturday" },
+];
+
+const animatedComponents = makeAnimated();
+
+const Day = (props) => {
+  const [daysOption, setDaysOption] = useState(null);
+  const dropdownChangeHandler = (option) => {
+    setDaysOption(option);
+    // mapping from option's object to array only contains value
+    props.onChangeFilter(option.map(option => option.value));
+  };
+
+  return (
+    <div className="row">
+      <label>{props.description}</label>
+      <SelectsContainer>
+        <Select
+          className="select"
+          closeMenuOnSelect={false}
+          allowClear
+          placeholder="Please Select"
+          isMulti="True"
+          options={day_options}
+          components={animatedComponents}
+          onChange={dropdownChangeHandler}
+        />
+      </SelectsContainer>
+    </div>
+  );
+};
+
+const SelectsContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  height: 20%;
+  width: 100%;
+`;
+
+export default Day;
