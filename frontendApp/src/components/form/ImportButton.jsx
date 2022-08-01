277cd009f19039532d86f2cda0e670917ac3c7dc
ay135 <ay135@duke.edu>
2022-08-01 19:21:07 -0400

    Upload New File

---
+++ frontendApp/src/components/form/ImportButton.jsx
@@ -0,0 +1,35 @@
+import React, { useRef, useDispatch } from "react";
+import { useHistory } from "react-router-dom";
+import "../../App.css";
+
+
+const ImportButton = (props) => {
+  const history = useHistory();
+  const dispatch = useDispatch();
+  const fileRef = useRef();
+  const [table_name, setTableName] = React.useState("");
+
+
+  return (
+    <div id="button" className="row">
+      <div className="buttonContainer">
+        <button
+          className="importButton"
+          onClick={() => fileRef.current.click()}
+        >
+          {props.title}
+        </button>
+        <input
+          ref={fileRef}
+          onChange={handleImport}
+          multiple={false}
+          type="file"
+          accept=".csv"
+          hidden
+        />
+      </div>
+    </div>
+  );
+};
+
+export default ImportButton;
