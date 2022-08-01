237c997ec799249c17bb987506bf571cdc44b396
ay135 <ay135@duke.edu>
2022-08-01 19:23:55 -0400

    Upload New File

---
+++ frontendApp/src/redux/store.js
@@ -0,0 +1,24 @@
+import { configureStore } from "@reduxjs/toolkit";
+import tableReducer from "./slices/tableSlice";
+import storage from "redux-persist/lib/storage/session";
+import { persistReducer, persistStore } from "redux-persist";
+import thunk from "redux-thunk";
+
+const persistConfig = {
+    key: 'root',
+    storage,
+};
+
+// replace the value of the reducer property from tableReducer to persistedReducer
+// enhaced reducer with configruation to persist the state to local storage
+const persistedReducer = persistReducer(persistConfig, tableReducer);
+
+export const store = configureStore(
+    {
+        reducer: persistedReducer,
+        devTools: process.env.NODE_ENV !== 'production',
+        middleware: [thunk]
+    }
+);
+
+export const persistor = persistStore(store);
